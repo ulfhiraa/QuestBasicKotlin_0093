1fa51f5afa2ee73a0c0fbf3fb1a45244eaa9caa8
ulfhiraa <studyiraa@gmail.com>
2024-10-12 15:32:25 +0700

    CONDITIONAL EXPRESSION dengan a. if-else b. when c. try-catch

---
+++ app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
@@ -0,0 +1,9 @@
+package com.example.basickotlin
+
+/*
+Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+menggunakan :
+a) if-else
+b) when
+c) try-catch
+*/
