d58b1e6ab00b6268cf4002f1b5807478b2f5f91c
ulfhiraa <studyiraa@gmail.com>
2024-10-12 15:21:29 +0700

    MAP MUTABLE : Data dapat diubah (menggunakan mutableMapOf)

--- app/src/main/java/com/example/basickotlin/Collections.kt
+++ app/src/main/java/com/example/basickotlin/Collections.kt
@@ -92,4 +92,8 @@ fun ContohMap() {
     // MAP Read-Only
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
+
+    // MAP Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
 }
