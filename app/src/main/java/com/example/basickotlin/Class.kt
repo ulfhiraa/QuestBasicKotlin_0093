badcb37183a6cdb8c0d49cc52754253feae2e988
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:13:44 +0700

    CLASS kosong tanpa atribut

---
+++ app/src/main/java/com/example/basickotlin/Class.kt
@@ -0,0 +1,9 @@
+package com.example.basickotlin
+
+/* CLASS
+Objek berguna untuk menyimpan data dalam program.
+Class memungkinkan kita untuk mendeklarasikan sekumpulan karakteristik untuk sebuah objek
+*/
+
+// class kosong yang tidak memiliki atribut.
+class Motor() // class dengan objek Motor
