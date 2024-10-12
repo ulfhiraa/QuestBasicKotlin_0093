7d871454c35aab35536773d3ea61500cc8767cb6
ulfhiraa <studyiraa@gmail.com>
2024-10-12 15:20:49 +0700

    MAP READ-ONLY : Data tidak dapat diubah (menggunakan mapOf)

--- app/src/main/java/com/example/basickotlin/Collections.kt
+++ app/src/main/java/com/example/basickotlin/Collections.kt
@@ -84,3 +84,12 @@ a) Unordered -> data yang dimasukkan tidak memiliki posisi tertentu
 b)  - Mutable : data dapat diubah (mutableMapOf)
     - Read Only : data tidak dapat diubah (mapOf)
 */
+
+fun ContohMap() {
+    println()
+    println("=== MAP ===")
+
+    // MAP Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+}
