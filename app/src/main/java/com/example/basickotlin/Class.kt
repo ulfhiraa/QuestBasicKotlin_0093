5cd6be321e0a01bc1e9692e7a481d8fbb3323e49
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:15:45 +0700

    CLASS yang memiliki atribut

--- app/src/main/java/com/example/basickotlin/Class.kt
+++ app/src/main/java/com/example/basickotlin/Class.kt
@@ -7,3 +7,8 @@ Class memungkinkan kita untuk mendeklarasikan sekumpulan karakteristik untuk seb
 
 // class kosong yang tidak memiliki atribut.
 class Motor() // class dengan objek Motor
+
+/* Class yang didefinisikan dan memiliki paremeter
+class dengan objek Contact, dengan 2 parameter : id dan email.
+val : data tidak dapat diubah | var : data dapat diubah */
+class Contact(val id: Int, var email: String)
