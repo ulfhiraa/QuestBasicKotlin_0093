e466f2b1672d721d44131ea0adf29af49bd857f9
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:27:40 +0700

    NULL SAFETY

---
+++ app/src/main/java/com/example/basickotlin/NullSafety.kt
@@ -0,0 +1,15 @@
+package com.example.basickotlin
+
+/* NULL SAFETY
+Mendeteksi potensi masalah dengan nilai null pada saat kompilasi,
+bukan saat dijalankan.
+
+Null safety adalah kombinasi dari fitur-fitur yang memungkinkan Anda untuk:
+-	secara eksplisit menyatakan kapan nilai null diperbolehkan dalam program Anda.
+-	memeriksa nilai null.
+-	menggunakan pemanggilan yang aman ke properti atau fungsi yang mungkin berisi nilai null.
+-	mendeklarasikan tindakan yang harus diambil jika nilai null terdeteksi.
+
+    Dengan mendeklarasikan variabel sebagai String?, kamu bisa menyimpannya dengan nilai null,
+    yang sangat berguna dalam situasi di mana data mungkin tidak tersedia.
+*/
