fb03d178efd0191519a07d6558d97246f9cfa327
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:00:21 +0700

    FUNCTION tanpa parameter

---
+++ app/src/main/java/com/example/basickotlin/Function.kt
@@ -0,0 +1,10 @@
+package com.example.basickotlin
+
+// FUNCTION
+// fungsi tanpa parameter
+fun withoutParameter() {
+    println("FUNCTION")
+    println("")
+    println("== WITHOUT PARAMETER ==")
+    println("Hello, World!")
+}
