d3331444c43c92467de150b697d5207ee9570c2a
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:29:55 +0700

    NULL SAFETY var nullable yang boleh bernilai null karena ada tanda tanya '?'

--- app/src/main/java/com/example/basickotlin/NullSafety.kt
+++ app/src/main/java/com/example/basickotlin/NullSafety.kt
@@ -21,4 +21,8 @@ fun nullSafety(){
     // Throws a compiler error
     // neverNull = null
 
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    // This is OK
+    nullable = null
 }
