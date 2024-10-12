f77014e8ce3508c352e206afeb50cc04c0c91cae
ulfhiraa <studyiraa@gmail.com>
2024-10-12 16:02:40 +0700

    NAMED ARGUMENT with Named Argument : menyertakan nama parameter yang membuat kode lebih mudah dibaca.

--- app/src/main/java/com/example/basickotlin/Function.kt
+++ app/src/main/java/com/example/basickotlin/Function.kt
@@ -19,3 +19,15 @@ fun withParameter(name: String) {
     println("== WITH PARAMETER ==")
     println("Hello, $name!")
 }
+
+/* NAMED ARGUMENT
+Cara untuk memanggil fungsi dengan menyebutkan nama parameter
+menyertakan nama parameter akan membuat kode Anda lebih mudah dibaca. Ini disebut menggunakan ARGUMEN BERNAMA.
+Jika menyertakan nama parameter, maka kita dapat menulis parameter dalam urutan apa pun.
+*/
+
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== with NAMED ARGUMENT ==")
+    println("Hello, name$! You are $age years old")
+}
