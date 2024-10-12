f0d88175a67ee0e8ea2d00c7560275c24a1758f3
ulfhiraa <studyiraa@gmail.com>
2024-10-12 09:10:19 +0700

    Collections : LIST. Menyimpan data urut sesuai penambahannya, memungkinkan duplikat data.

--- app/src/main/java/com/example/basickotlin/Collections.kt
+++ app/src/main/java/com/example/basickotlin/Collections.kt
@@ -1,2 +1,8 @@
 package com.example.basickotlin
 
+// LIST
+/* list adalah Kumpulan data yang disusun secara berurutan
+* a) bersifat ORDERED -> data yang dimasukkan akan memiliki posisi yang urut
+* MUTEABLE -> data yang dimasukkan dapat diubah (listof)
+* READ-ONLY -> data tidak dapat diubah (mutableListof)
+*/
